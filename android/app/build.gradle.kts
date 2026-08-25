ee4312741ea972b81b1c12425019b2bdaa10b5e8
Vitalii <vitalii87@users.noreply.github.com>
2026-08-25 10:34:30 +0200

    Finish Android APK packaging

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,11 @@ android {
     namespace = "com.swagri.android"
     compileSdk = 35
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     defaultConfig {
         applicationId = "com.swagri.android"
         minSdk = 29
